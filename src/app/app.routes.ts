3d746c9848d8eaeb981eb1a8d8cfda86ffe5c754
MahmoudKh <mahmoudkhair000@outlook.com>
2025-03-05 23:14:17 +0200

    started home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../components/home/home.component';
 import { AboutComponent } from '../components/about/about.component';
+import { ContactComponent } from '../components/contact/contact.component';
+import { ServicesComponent } from '../components/services/services.component';
 
 export const routes: Routes = [
     {
@@ -9,8 +11,35 @@ export const routes: Routes = [
         component: HomeComponent,
     },
     {
+        title: "Services",
+        path: "services",
+        component: ServicesComponent,
+    },
+/*
+    {
         title: "About",
         path: "about",
         component: AboutComponent,
+    },
+    {
+        title: "About",
+        path: "about",
+        component: AboutComponent,
+    },
+    {
+        title: "About",
+        path: "about",
+        component: AboutComponent,
+    },
+*/
+    {
+        title: "About Us",
+        path: "about",
+        component: AboutComponent,
+    },
+    {
+        title: "Contact Us",
+        path: "contact",
+        component: ContactComponent,
     }
 ];
--- src/components/services/services.component.ts
+++ src/components/services/services.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../footer/footer.component";
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-services',
-  imports: [],
+  imports: [FooterComponent, NavbarComponent],
   templateUrl: './services.component.html',
   styleUrl: './services.component.css'
 })
