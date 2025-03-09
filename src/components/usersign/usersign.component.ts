c1f4476473d8da1b35da5148f00b610b81effea3
MahmoudKh <mahmoudkhair000@outlook.com>
2025-03-09 18:33:46 +0200

    about done

--- src/components/usersign/usersign.component.ts
+++ src/components/usersign/usersign.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
 import { FooterComponent } from '../footer/footer.component';
+import { NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-usersign',
-  imports: [NavbarComponent,FooterComponent],
+  imports: [NavbarComponent,FooterComponent, CommonModule],
   templateUrl: './usersign.component.html',
   styleUrl: './usersign.component.css'
 })
